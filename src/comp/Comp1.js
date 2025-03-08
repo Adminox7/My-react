259b97550f1e07a60412bf0c3f412436207bfc4b
Adminox7 <adammait34@gmail.com>
2025-03-08 10:56:16 +0000

    first commit

--- src/comp/Comp1.js
+++ src/comp/Comp1.js
@@ -9,7 +9,7 @@ function comp1() {
         <img src="./img/MON PHOTO.jpeg" alt="mon image" />
         <div className="about-text">
           <h2>About Me</h2>
-          <h5>Developer <span>& Designer</span></h5>
+          <h5>Front end <span>& Back end </span> Developer</h5>
           <p>
             Hello, I am Adam, I live in Morocco. I hold a programming certificate from vocational training, and I have
             received three months of training at rais_eau company in Arduino programming. I can also create websites with
